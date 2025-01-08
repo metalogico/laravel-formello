0b776db2f017b4d1ba473b952344caaeb8a71f5d
metalogico <michele.brandolin@gmail.com>
2025-01-08 11:40:35 +0100

    fix: improved checkboxes widget

--- resources/views/widgets/bootstrap5/checkboxes.blade.php
+++ resources/views/widgets/bootstrap5/checkboxes.blade.php
@@ -3,19 +3,19 @@
         <label for="{{ $config['attributes']['id'] }}" class="form-label">{{ $label }}</label>
     @endif
 
-    @foreach($choices as $option)
+    @foreach($choices as $optionValue => $optionLabel)
         <div class="form-check mb-3">
             <input
                 @foreach ($config['attributes'] as $attr => $attrValue) {{ $attr }}="{{ $attrValue }}" @endforeach
                 type="checkbox"
                 name="{{ $name }}[]"
-                value="{{ $option['value'] }}"
+                value="{{ $optionValue }}"
                 class="{{ $attributes['class'] ?? '' }} @if ($errors) is-invalid @endif"
-                id="{{ $name }}_{{ $option['value'] }}"
-                {{ in_array($option['value'], (array)old($name, $value)) ? 'checked' : '' }}
+                id="{{ $name }}_{{ $optionValue }}"
+                {{ in_array($optionValue, (array)old($name, $value)) ? 'checked' : '' }}
             >
-            <label class="form-check-label" for="{{ $name }}_{{ $option['value'] }}">
-                {{ $option['label'] }}
+            <label class="form-check-label" for="{{ $name }}_{{ $optionValue }}">
+                {{ $optionLabel }}
             </label>
         </div>
     @endforeach
