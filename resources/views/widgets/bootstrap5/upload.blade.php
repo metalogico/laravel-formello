3ca682f16c163e63c19f7ad6127ffdd584b48da7
metalogico <michele.brandolin@gmail.com>
2025-07-09 16:09:30 +0200

    typo: again

--- resources/views/widgets/bootstrap5/upload.blade.php
+++ resources/views/widgets/bootstrap5/upload.blade.php
@@ -1,16 +1,16 @@
 <div class="form-group mb-3"> 
     
     <div class="custom-file">
+        @if (isset($label))
+            <label for="{{ $config['attributes']['id'] }}" class="form-label">{{ $label }}</label>
+        @endif
+
         <input type="{{ $config['attributes']['type'] }}" name="{{ $name }}" value="{{ $value }}" 
             class="{{ $config['attributes']['class'] }} @if ($errors) is-invalid @endif"
             @foreach ($config['attributes'] as $attr => $attrValue) {{ $attr }}="{{ $attrValue }}" @endforeach>
         
         @if (isset($config['help']))
             <div class="form-text">{!! $config['help'] !!}</div>
-        @endif
-
-        @if (isset($label))
-            <label for="{{ $config['attributes']['id'] }}" class="custom-file-label">{{ $label }}</label>
         @endif
     </div>
 
