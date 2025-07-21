83694655b6dfc0c5415d6ef09c63bd46e656826a
metalogico <michele.brandolin@gmail.com>
2025-07-21 11:24:02 +0200

    fix: better toggle rendering

--- resources/views/widgets/bootstrap5/toggle.blade.php
+++ resources/views/widgets/bootstrap5/toggle.blade.php
@@ -1,13 +1,13 @@
-<div class="form-check form-switch mb-3">
-
-    <input type="hidden" name="{{ $name }}" value="0"> <!-- for the unchecked value -->
-    <input name="{{ $name }}" value="1"
-        @foreach ($config['attributes'] as $attr => $attrValue)
-            {{ $attr }}="{{ $attrValue }}" @endforeach>
+<div class="form-group mb-3">
     @if (isset($config['label']))
-        <label class="form-check-label" for="{{ $config['attributes']['id'] }}">{{ $config['label'] }}</label>
+        <label class="form-label" for="{{ $config['attributes']['id'] }}">{{ $config['label'] }}</label>
     @endif
-    
+    <div class="form-check form-switch">
+        <input type="hidden" name="{{ $name }}" value="0"> <!-- for the unchecked value -->
+        <input name="{{ $name }}" value="1"
+            @foreach ($config['attributes'] as $attr => $attrValue)
+                {{ $attr }}="{{ $attrValue }}" @endforeach>
+    </div>
     @if (isset($config['help']))
         <div class="form-text">{!! $config['help'] !!}</div>
     @endif
